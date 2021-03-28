089f8cf377eea60dbf2ee5ccce4c554d273379bb
thutruong84 <71114216+thutruong84@users.noreply.github.com>
2021-03-27 21:30:08 -0700

    Create name lists, function for searching bday, and capitalize name

--- birthdayExample.py
+++ birthdayExample.py
@@ -26,13 +26,64 @@ for elem in birthdayList:
 
     birthdayDictionary[name] = birthday
 
-# get user input
+# create a list of full names
+fullName = list(birthdayDictionary.keys())
+
+# create lists of first names and last names only
+firstName = []
+lastName = []
+
+for j in fullName:
+    firstName.append(j.split()[0])
+    lastName.append(j.split()[1])
+
+# Create a function searchName that takes in a name and a list of a name (full, first, or last) to be used
+# Function returns the birthday(s) for the input name if the name exists in the birthday dictionary
+# Function returns nothing if the name does not exist in birthday dictionary
+def searchName(name,nameList):
+
+    #print value in the dictionary by giving it a string name as the key if the name exists
+    if name in nameList:
+        print("Here is the list of name(s) and birthday(s) you are searching for:")
+        for index in range(len(nameList)):
+            if nameList[index] == name:
+                entireName = fullName[index]
+                print(entireName + " " + birthdayDictionary[entireName])
+            
+    # print no information found if name is not in the dictionary 
+    else:
+        print("No information found for " + name)
+
+
+
+# display menu option and get user input
+
+#print("---Enter a number to select search option or 'q' to quit---")
+#print("[1] Search by full name")
+#print("[2] Search by last name")
+#print("[3] Search by first name")
+#print("[q] Quit this program")
+
+#opt = input("Choose an option:")
+
+# create loop and if-else statment
+#while (opt != "q"):
+
+
+
+
+
 name = input("Enter a name:")
 
-# print value in the dictionary by giving it a string name as the key if the name exists
-if name in birthdayDictionary:
-    print(name +  "'s birthday is: " + birthdayDictionary[name])
-
-# print no information found if name is not in the dictionary    
+# process name to capitalize first letter of full name, first name, or last name
+if " " in name:
+    first = name.split(" ")[0]
+    last = name.split(" ")[1]
+    name = first.capitalize() + " " + last.capitalize()
 else:
-    print("No information for " + name + " found.")
+    name = name.capitalize()
+
+
+
+
+    
